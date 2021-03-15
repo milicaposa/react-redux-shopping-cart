0503b77f7bfd870b38ba9938e6e5c1fb86de63e0
milicaposa <35407428+milicaposa@users.noreply.github.com>
2021-03-15 11:14:16 +0100

    fixing warnings in console.log

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./App.scss";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById("root")
 );
